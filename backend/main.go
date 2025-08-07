11f7b1292512eca761e1ea3190588038708b9a9d
Yoko <68815364+ZekiTRR@users.noreply.github.com>
2025-08-07 09:17:11 +0300

    Docker and main file update
    Minor update affecting docker compose and a placeholder added to the main backend file

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,57 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	ID           uint      `gorm:"primaryKey"`           // Уникальный идентификатор
+	Username     string    `gorm:"uniqueIndex;not null"` // Имя пользователя, уникальное и обязательное
+	Email        string    `gorm:"uniqueIndex;not null"` // Email пользователя, уникальный и обязательный
+	PasswordHash string    `gorm:"not null"`             // Хэш пароля, обязательный
+	CreatedAt    time.Time // Время создания записи
+}
+
+var DB *gorm.DB
+
+func Init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"),
+	)
+
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db.AutoMigrate(&User{})
+
+	fmt.Println("Connected to the database successfully!")
+	DB = db
+}
+
 func main() {
-	router := gin.Default()
+    Init() // Временно, просто заглушка
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run(":8080")
 }
